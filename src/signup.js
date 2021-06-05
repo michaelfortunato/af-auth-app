2b6ba1aee6ed20dadc0cef31398b5b6c01732331
Michael Fortunato <michael.n.fortunato@gmail.com>
2021-06-05 18:05:30 -0400

    good

--- src/signup.js
+++ src/signup.js
@@ -6,35 +6,38 @@ const router = express.Router();
 
 const saltRounds = 10;
 const verificationExpirationSeconds = 3600;
-router.use("/", async (req, res, next) => {
-  try {
-    const email = req.body.email.toLowerCase();
 
+const isUserVerified = async (req, res, next) => {
+  try {
+    email = req.body.email.toLowerCase();
     const accountCollection =
       req.app.locals.database.collection("Verified-Accounts");
-    const accountAlreadyExistsPromise = accountCollection.findOne({
+    const accountAlreadyExists = await accountCollection.findOne({
       _id: email,
       email: email,
     });
-    const hashedPasswordPromise = bcrypt.hash(req.body.password, saltRounds);
-
-    const [accountAlreadyExists, hashedPassword] = await Promise.all([
-      accountAlreadyExistsPromise,
-      hashedPasswordPromise,
-    ]);
-
     if (accountAlreadyExists) {
       return res
         .status(401)
         .send({ statusMessage: "Email already registered to Art-Flex" });
     }
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const hashPassword = async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
     res.locals.hashedPassword = hashedPassword;
     next();
   } catch (error) {
     next(error);
   }
-});
-router.use("/", async (req, res, next) => {
+};
+const signUpUser = async (req, res, next) => {
   // Post to cache
   // Post to database
   // If either of these fail rollback the transaction
@@ -49,35 +52,104 @@ router.use("/", async (req, res, next) => {
     const email = req.body.email.toLowerCase();
     //If someone previously attempted a sign up but did not complete verification,
     //then we want to replace their sign up entry.
-    const vertificationToken = crypto.randomBytes(48).toString("hex");
+    const verificationToken = crypto.randomBytes(48).toString("hex");
     signUpCollection.createIndex(
       { verificationToken: 1 },
       { expireAfterSeconds: verificationExpirationSeconds },
       { unique: true }
     );
 
+    // Notice the tokenCreatedAt field,
+    // its a safe guard in case mongodb forgets to delete the entry after expiration
     await signUpCollection.replaceOne(
       { _id: email, email: email },
       {
         _id: email,
         email: email,
         password: res.locals.hashedPassword,
-        verificationToken: vertificationToken,
+        verificationToken: verificationToken,
+        tokenCreatedAt: new Date().getTime(),
       },
       { upsert: true }
     );
 
     res.locals.body = {
       email: email,
-      verificationToken: vertificationToken,
+      verificationToken: verificationToken,
     };
     next();
   } catch (error) {
     next(error);
   }
-});
+};
 
-router.use("/", (error, req, res, next) => {
+router.post("/new", [isUserVerified, hashPassword, signUpUser], (req, res) => {
+  return res.status(200).send(res.locals.body);
+});
+const verifyUser = async (req, res, next) => {
+  try {
+    res.locals.email = req.body.email.toLowerCase();
+    const signUpCollection = req.app.locals.database.collection("SignUpForms");
+    res.locals.user = await signUpCollection.findOne({
+      _id: res.locals.email,
+      email: res.locals.email,
+      verificationToken: req.params.token,
+    });
+    if (!res.locals.user) {
+      return res.status(401).send({
+        statusMessage:
+          "Could not verify account. Form has not been filled out.",
+      });
+    }
+
+    // Check to make sure the current time minus the tokenCreatedAt entry is less than the expiration time
+    const currentTime = new Date().getTime();
+    const timeElapsed = (currentTime - res.locals.user.tokenCreatedAt) / 1000; // Convert from ms to s
+    const isValid =
+      timeElapsed < verificationExpirationSeconds && timeElapsed >= 0; // in case the current time is off for some reason
+    console.log(currentTime);
+    console.log(res.locals.user.tokenCreatedAt);
+    if (!isValid) {
+      return res.status(401).send({
+        statusMessage:
+          "Could not verify account. Verification token is expired",
+      });
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const addUserToVerifiedAccounts = async (req, res, next) => {
+  try {
+    const accountCollection =
+      req.app.locals.database.collection("Verified-Accounts");
+    await accountCollection.insertOne({
+      _id: res.locals.user.email,
+      email: res.locals.user.email,
+      password: res.locals.user.password,
+      role: res.locals.user.role,
+    });
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+router.post(
+  "/verify/:token",
+  [isUserVerified, verifyUser, addUserToVerifiedAccounts],
+  (req, res) => {
+    return res
+      .status(200)
+      .send({ statusMessage: "Successfully verfied user. Account created." });
+  }
+);
+
+router.use((error, req, res, next) => {
   // If the error code is 11000,
   // then it is a duplicate key, and the account already exists
   if (error.code === 11000) {
@@ -85,10 +157,6 @@ router.use("/", (error, req, res, next) => {
   } else {
     return res.status(500).send({ statusMessage: error.message });
   }
-});
-
-router.post("/", (req, res) => {
-  return res.status(200).send(res.locals.body);
 });
 
 module.exports = router;
